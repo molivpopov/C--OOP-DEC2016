5f9fa357310c41ceac8ff38e83baaa50c1285428
molivpopov <mitkop@mail.bg>
2016-12-27 09:00:06 +0200

    minor Fixes On Part1 and new Par2

---
+++ Homework/DefiningClassesPart2/DefiningClassesPart2/PathInSpace/Point3D.cs
@@ -0,0 +1,14 @@
+﻿namespace PathInSpace
+{
+    public struct Point3D
+    {
+        private const string PatternToPrint = "X={0:f2}, Y={1:f2}, Z={2:f2}\n";
+        public double X;
+        public double Y;
+        public double Z;
+        public override string ToString()
+        {        
+            return string.Format(PatternToPrint, X, Y, Z); ;
+        }
+    }
+}
